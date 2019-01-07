4a8bb4910f0300def692b9727af0ab1f5b74d84d
MichaelDuo <michaeldong21@gmail.com>
2019-01-07 15:37:16 +0800

    Initial Commit

---
+++ precache-manifest.af0727ccae69dff521334183507aacbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f7ccc3188ddbf2b1667",
+    "url": "/static/js/main.1f7ccc31.chunk.js"
+  },
+  {
+    "revision": "a531ac058ca2171aaf43",
+    "url": "/static/js/1.a531ac05.chunk.js"
+  },
+  {
+    "revision": "1f7ccc3188ddbf2b1667",
+    "url": "/static/css/main.daffb034.chunk.css"
+  },
+  {
+    "revision": "a531ac058ca2171aaf43",
+    "url": "/static/css/1.79b8b4cf.chunk.css"
+  },
+  {
+    "revision": "46f06e5830fb3fc253969bfd08427538",
+    "url": "/index.html"
+  }
+];
