68b555f36375bafaa5a5405bbefd9c56f50e099c
Bot <michaeldong21@gmail.com>
2019-01-07 10:19:21 +0000

    Deployment #9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac86fd52f72121e093047641c25b70f8.js"
+  "/precache-manifest.7a491136017dd45476dcd084e51ae4bb.js"
 );
 
 workbox.clientsClaim();
